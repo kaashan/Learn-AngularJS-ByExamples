bd0521288d07e2143f6d46384fa5bc5c9175fc52
kaashan <hussain.kaashan99@gmail.com>
2016-09-17 00:58:02 +0530

    Using Services

--- BloodBankLocator/js/controllers.js
+++ BloodBankLocator/js/controllers.js
@@ -1,6 +1,6 @@
 var appControllers = angular.module('appControllers', []);
 
-appControllers.controller('MainController',['$scope','$http', function($scope,$http){
+appControllers.controller('MainController',['$scope','$http','JSONService', function($scope,$http,JSONService){
 	$scope.url = 'https://data.gov.in/api/datastore/resource.json?resource_id=e16c75b6-7ee6-4ade-8e1f-2cd3043ff4c9&api-key=317c2c3b801ff9afdc0e9a3b36249b47';
 	
 	$scope.findBanks = function(){
@@ -11,31 +11,28 @@ appControllers.controller('MainController',['$scope','$http', function($scope,$h
 		if($scope.city){
 			url = url +"&filters[city]="+ $scope.city;
 		}
-		
 		$http.get(url).then(function(response) {
 			if(response.data.records.length){
 				self.listData = response.data.records;
 				self.noRecordsFound = false;
 			}else{
 				self.noRecordsFound = true;
-			}
-			
+			}	
 		});
 	}
 
 	$scope.getCities = function(){
 		var cityList = document.getElementById('cityDrpdwn');
 		$scope.clearList(cityList);
-		$http.get('data/' + $scope.state + '-cities.json').then(function(response) {
-			var jsonOptions = response.data;
-			jsonOptions.forEach(function(item) {
-		        var option = document.createElement('option');
-		        option.value = item.name;
-				var optionTxt = document.createTextNode(item.city);
-		        option.appendChild(optionTxt);
-		        cityList.appendChild(option);
-	      });
-		});
+		var jsonOptions = JSONService.getJSONFile($scope.state + '-cities.json');
+		jsonOptions.then(
+			function(response){ //success callback
+				JSONService.populateData(response.data, cityList, 'city');
+			},
+			function(response){ //error callback
+				console.log('some error occurred');
+			}
+		);
 		$scope.city = "";
 	}
 
